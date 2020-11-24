4fd383fb3c3ee03990038a3fe1a7023587499e0b
Nicolas Toporcov <ntoporcov@me.com>
2020-11-23 20:11:11 -0500

    adding favicons

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,10 +1,7 @@
 import React from "react";
-import {createUseStyles} from "react-jss";
 import BigHero from "../components/heroes/bigHero";
 import TeamSummary from "../components/misc/teamSummaryRow";
 import TeamDetails from "../components/misc/teamDetails";
-
-const useStyles = createUseStyles({})
 
 const Team = () => {
 
